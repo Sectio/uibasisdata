6c7e95be7d334cfa04e3f1c299ea05aa2f351fd1
Sectio <ramadhani2910712@gmail.com>
2020-11-27 21:21:56 +0700

    first commit

---
+++ kk4-html/edit.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>The Colection</title>
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>WELCOME</title>
+		<link rel="stylesheet" type="text/css" href="css/style1.css">
+		<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+  </head>
+
+  <body>
+    <header style="background: #000000;">
+			<div class="container">
+				<h1><a>BLACK MARKET</a></h1>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="menu.html">Colection</a></li>
+					<li><a href="contact-us.html">Order Now</a></li>
+				</ul>
+			</div>
+		</header>
+
+    <input type="checkbox" id="check">
+    <div class="sidebar">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="menu.html">Colection</a></li>
+        <li><a href="contact-us.html">Order Now</a></li>
+      </ul>
+    </div>
+
+    <section class="service">
+    <div class="container">
+      <button><a href="index.php">Tambah Menu</a></button>
+        <br/><br/>
+
+        <form action="add.php" method="post" name="form1">
+            <table width="25%" border="0">
+          </table>
+      </form>
+    </div>
+  </section>
+
+
+		<footer>
+			<div class="container">
+				<small>Copyright &copy; 2020 - Sectio Kautsar Ramadhani</small>
+			</div>
+		</footer>
+
+  </body>
+</html>
